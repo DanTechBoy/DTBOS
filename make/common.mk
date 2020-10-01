cf44c78fba6d7e39ffeed071861e913d402bd449
Daniel <danielhammer92@outlook.com>
2020-10-01 16:29:22 +0400

    Update makefiles

--- make/common.mk
+++ make/common.mk
@@ -6,8 +6,8 @@ tags:
 
 ##     clean: clears generated files
 clean:
-	@rm -rf $(ISO) iso odyssey.img odyssey-coverity.tar.gz cov-int
-	@rm -f odyssey odyssey.map odyssey.bin
+	@rm -rf $(ISO) iso dtbos.img odyssey-coverity.tar.gz cov-int
+	@rm -f odyssey dtbos.map odyssey.bin
 	@find . -not -path "./deps/*" -not -path "./.git/*" \
 		\( -name '*.o' -o -name '*.d' -o -name '*.bin' \) \
 		-exec rm {} \;
